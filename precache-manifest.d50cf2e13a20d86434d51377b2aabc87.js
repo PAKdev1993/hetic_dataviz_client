2fc5028d3a5c49154a3abaa7e6be5e818a2b48b3
PAKdev1993 <pakrstic.git@gmail.com>
2019-06-20 02:15:13 +0200

    FIRST COMMIT

---
+++ precache-manifest.d50cf2e13a20d86434d51377b2aabc87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3ca3ae6435ce83a826683501e72f8e4",
+    "url": "/hetic_dataviz_client/index.html"
+  },
+  {
+    "revision": "03788215d2e3ae6f8ab5",
+    "url": "/hetic_dataviz_client/static/css/2.7ff19518.chunk.css"
+  },
+  {
+    "revision": "c902a7b42f5fb480e580",
+    "url": "/hetic_dataviz_client/static/css/main.b10c820d.chunk.css"
+  },
+  {
+    "revision": "03788215d2e3ae6f8ab5",
+    "url": "/hetic_dataviz_client/static/js/2.1cc5834d.chunk.js"
+  },
+  {
+    "revision": "c902a7b42f5fb480e580",
+    "url": "/hetic_dataviz_client/static/js/main.a5d0d155.chunk.js"
+  },
+  {
+    "revision": "45810f0a4586d78f6f5b",
+    "url": "/hetic_dataviz_client/static/js/runtime~main.1ee4b60e.js"
+  }
+]);
--- src/styles/theme.js
+++
@@ -1,6 +0,0 @@
-export default {
-  color1: "#377C4E",
-  color2: "rgba(73, 180, 98, 0.8)",
-  color3: "#FEFBEC",
-  color4: "#5E9570"
-};
